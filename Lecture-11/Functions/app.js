1dfdc36994e08ede11e585c296c1e39479d11a37
Sabeel Khan <sabeelhps@gmail.com>
2021-09-20 20:42:33 +0530

    Lecture-11

---
+++ Lecture-11/Functions/app.js
@@ -0,0 +1,67 @@
+
+// // function defintion
+// function fun() {
+//     console.log('Hello fron function fun');
+// }
+
+// // function call
+// fun();
+
+
+// function add(x, y, z) {
+//     // console.log(x + y + z);
+
+//     return x + y + z;
+// }
+
+// const ans = add(2, 3, 4);
+// console.log(ans);
+
+
+// function subtract(a, b=2,c) {
+    
+//     return a - b - c;
+// }
+
+// const res = subtract(10, 4);
+// console.log(res);
+
+
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// console.log(add(2, 3));
+// console.log(add('Hello', 'World'));
+// console.log(add(10, 'Hello'));
+// console.log(add(true, 10));
+// console.log(add(false, 'Hello'));
+// console.log(add("", 10));
+// console.log(add([], 15));
+
+// functional expression 
+// const f = function fun() {
+//     console.log('Inside fun');
+// }
+
+// Anonymous function as function expression
+// const f = function() {
+//     console.log('Inside fun');
+// }
+
+// console.log(f);
+// f();
+
+// --------------------------------------------
+
+var x = 100;
+
+function fun() {
+    var y = 99;
+    console.log('Inside fun');
+    console.log(y);
+}
+
+console.log(x);
+fun();
---
+++ Lecture-11/JS-Loops/app.js
@@ -0,0 +1,97 @@
+//Looops
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for (initilisation; condition check ; updation) {
+    
+// }
+
+// let i = 1;
+
+// for (; i <= 10;){
+//     console.log(i); i++; 
+// }
+
+// for (let i = 100; i <= 200; i -= 20){
+//     console.log(i)
+// }
+
+
+// while (condition) {
+//     // statements
+// }
+
+// let i = 100;
+
+// while (i >= 0) {
+//     console.log(i);
+//     i -= 10;
+// }
+
+
+
+// break and continue statements in Loops
+
+// for (let i = 1; i <= 10; i++){
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 5; i++){
+//     for (let j = 0; j <= 5; j++){
+//         if (i === j) {
+//             break;
+//         }
+//         console.log(i, j);
+//     }
+// }
+
+// for (let i = 0; i <= 10; i++){
+    
+//     if (i === 5) {
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+
+// for of Loops
+
+// const colors = ['Red', 'Green', 'Orange', 'Blue'];
+
+// let allColors = "";
+
+// for (let color of colors) {
+//     allColors +=color+' ';
+//     console.log(color);
+// }
+
+// console.log(allColors)
+
+
+// const car = {
+//     name: 'Audi',
+//     price: 20000,
+//     isDiskBrake: true,
+// }
+
+// for (let i in car) {
+//     console.log(`${i}--->${car[i]}`);
+// }
+
+
+
+
+
