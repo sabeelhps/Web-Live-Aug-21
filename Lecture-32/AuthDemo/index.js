36f1bb9f2f2a90c565324e177af64b9b5ba7cb44
Sabeel Khan <sabeelhps@gmail.com>
2021-11-23 20:44:40 +0530

    Lecture-32

---
+++ Lecture-32/AuthDemo/index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+const session = require('express-session')
+
+mongoose.connect('mongodb://localhost/authDemo')
+    .then(() => console.log('connection open'))
+    .catch((err) => console.log(err));
+
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.send('YOU NEED TO LOGIN FIRST');
+    }
+    next();
+}
+
+app.get('/register', (req, res) => {
+    
+    res.render('signup');
+});
+
+app.post('/register', async (req, res) => {
+    
+    const { username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(password, salt);
+
+
+    const newUser = new User({ username, password: hash });
+
+    await newUser.save()
+
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    
+    
+
+    res.render('login');
+});
+
+
+app.post('/login', async(req, res) => {
+    
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+       const validUser =  await bcrypt.compare(password,user.password)
+        if (!validUser) {
+           return res.send('Invalid Password')
+       }
+
+        req.session.user_id = user._id;
+    }
+
+
+    res.redirect('/secret');
+})
+
+
+app.get('/secret',requireLogin, (req, res) => {
+
+  
+    res.send('This is a Secret.You need to login first to access it!!!');
+});
+
+
+app.get('/logout', (req, res) => {
+    
+    if (req.session.user_id) {
+        req.session.destroy();
+        return res.redirect('/login');
+    }
+
+    res.send('YOU ARE NOT LOGGED IN YET!!!');
+})
+
+
+
+
+app.listen(3000,()=>{
+  console.log('server started at port 3000');
+});
---
+++ Lecture-32/AuthDemo/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
