8ecf2f6ebea5c427f3eaa5a629b170a0088b1245
Sabeel Khan <sabeelhps@gmail.com>
2021-12-16 21:28:34 +0530

    Lecture-41

--- Lecture-41/my-app/src/Components/First.js
+++ Lecture-41/my-app/src/Components/First.js
@@ -12,16 +12,31 @@ function getMood() {
 
 const First = () => {
 
+    const todoList = todos.map((item) => {
+        return <li>{item}</li>
+    });
+
+   
+
+    const randomNum = Math.floor(Math.random() * 10) + 1;
+    // const magicNum = 7;
+    // let show = null;
+    // if (randomNum === magicNum) {
+    //     show=<img src="https://media2.giphy.com/media/2sXf9PbHcEdE1x059I/giphy.gif?cid=e1bb72ff5b7ca7bf48637959772e6344" />
+    // } else {
+    //     show=<p>Not a Magic Number</p>
+    // }
 
     return (
         <div>
-            <h2>{Math.floor(Math.random() * 10) + 1}</h2>
+            <h2>{randomNum}</h2>
             <h2>My Mood is : {getMood()}</h2>
             <ul>
-                {todos.map((item) => {
-                    return <li>{item}</li>
-                })}
+                {todoList}
             </ul>
+
+            {/* {randomNum===7 ?<img src="https://media2.giphy.com/media/2sXf9PbHcEdE1x059I/giphy.gif?cid=e1bb72ff5b7ca7bf48637959772e6344" /> : <p>Not a Magic Number</p>} */}
+            {randomNum===7  && <img src="https://media2.giphy.com/media/2sXf9PbHcEdE1x059I/giphy.gif?cid=e1bb72ff5b7ca7bf48637959772e6344" />}
         </div>
     )
 }
--- Lecture-41/my-app/src/Components/Person.js
+++ Lecture-41/my-app/src/Components/Person.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Person.css';
 
-const Person = (props) => {
+const Person = ({name,age,favColor}) => {
 
     return (
         <article className="person">
-            <h2>Name : {props.name} </h2>
-            <p>Age : {props.age }</p>
-            <p>FavColor : { props.favColor}</p>
+            <h2>Name : {name} </h2>
+            <p>Age : {age }</p>
+            <p>FavColor : {favColor}</p>
         </article>
     )
 }
